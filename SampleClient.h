38d075c70483e0adb5fb821a2961c4ec34b081a8
rijaldin <rijal_kash@yahoo.com>
2017-08-22 14:06:32 +0800

    Initial commit

---
+++ SampleClient.h
@@ -0,0 +1,6 @@
+void _WriteHeader();
+void _WriteFrame();
+void _WriteFooter();
+void __cdecl DataHandler();		// receives data from the server
+void __cdecl MessageHandler();		            // receives NatNet error mesages
+void resetClient();
